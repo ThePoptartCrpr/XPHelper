a56d2d5624e296190f6d4b2b0275a0ca0cf17ca5
ThePoptartCrpr <ThePoptartCrpr@users.noreply.github.com>
2018-01-14 13:25:15 -0800

    Added settings button to main menu
    literally the title

--- commands.js
+++ commands.js
@@ -1,6 +1,8 @@
 TriggerRegister.registerCommand("command_xph", "xph", "/xph");
 TriggerRegister.registerCommand("command_xphtest", "xphtest", "/xphtest");
 
+var xphSettingsClickable = new Message(ChatLib.clickable("&b[SETTINGS]", "run_command", "/xph gui", "&bManage XPHelper settings"));
+
 function command_xph() {
 	switch(arguments[0]) {
 		case "gui": xphOpenGui();
@@ -8,6 +10,8 @@ function command_xph() {
 	if (!arguments[0]) {
 		xphChat(xphBreak);
 		xphChat(xphCenter("&aXP&bHelper"));
+		xphChat();
+		xphChat(xphSettingsClickable);
 		xphChat();
 		xphChat(xphBreak);
 	}
