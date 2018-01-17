ea7ac998f4319cc7beceb54f001311b8801d98a0
ThePoptartCrpr <ThePoptartCrpr@users.noreply.github.com>
2018-01-17 10:52:55 -0800

    apparently im triple bad so this should fix it

--- functions.js
+++ functions.js
@@ -46,20 +46,55 @@ function xphLoadSettings(json, fileLocation, fileName) {
 
   if (tempSettings != null && tempSettings != "" && tempSettings != undefined) {
   //////////////////////////////////////////////////////////////////
-		for (a in json) {
-			for (x in a) {
-				for (e in tempSettings) {
-		      for (y in e) {
+		for (x in json) {
+			for (x2 in json[x]) {
+				for (y in tempSettings) {
+		      for (y2 in tempSettings[y]) {
 		        //Debug: print("x: " + x + " = " + json[x] + "   Y: " + y + " = " + tempSettings[y])
-		        if (x == y) {
-		          json[a][x] = tempSettings[e][y];
-		          //Debug: print("Works: new json[x] = " + json[x]);
+		        if (x2 == y2) {
+		          json[x][x2] = tempSettings[y][y2];
+							// ChatLib.chat(JSON.stringify(json[a]) + " ");
 		          break;
 		        }
 		      }
 				}
 	    }
 		}
+		/*for (x in json) {
+			for (x2 in x) {
+				for (e in tempSettings) {
+					ChatLib.chat(JSON.stringify(json[x][x2]));
+				}
+			}
+		}*/
+  //////////////////////////////////////////////////////////////////
+  }
+  return json;
+}
+
+function xphLoadSettings1D(json, fileLocation, fileName) {
+  /***
+  json: the settings json you want saved.
+  fileLocation: the folder in which the file will be saved to.
+  fileName: the name of the file, must also include ".json" at the end.
+  Usage:
+  yourSettings = loadSettings(yourSettings, folderName, fileName);
+  ***/
+  var tempSettings = JSON.parse(FileLib.read(fileLocation, fileName));
+
+  if (tempSettings != null && tempSettings != "" && tempSettings != undefined) {
+  //////////////////////////////////////////////////////////////////
+    for (x in json) {
+      for (y in tempSettings) {
+        //Debug: print("x: " + x + " = " + json[x] + "   Y: " + y + " = " + tempSettings[y])
+        if (x == y) {
+          json[x] = tempSettings[y];
+          //Debug: print("Works: new json[x] = " + json[x]);
+          break;
+        }
+
+      }
+    }
   //////////////////////////////////////////////////////////////////
   }
   return json;
